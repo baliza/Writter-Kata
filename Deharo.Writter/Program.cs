9dfdb747decb49e1aa44a82a0034d16943e08219
NdeHaro <ndeharo@gteam.es>
2021-09-12 19:21:07 +0200

    Basic Structure Project - Initi commit

---
+++ Deharo.Writter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deharo.Writter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
