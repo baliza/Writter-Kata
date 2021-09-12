5c738c191fc7b04247ce6545452cdf6abe6d5311
JavierSG <jsanudo@gteam.es>
2021-09-12 12:15:12 +0200

    Añadidas clases para los formatos "Json, txt, xml yml" e implementada interfaz de IFormatable

---
+++ WritterKatarinaLoL/Model/Format_Txt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WritterKatarinaLoL.Interface;
+
+namespace WritterKatarinaLoL.Model
+{
+    class Format_Txt : IFactorable
+    {
+    }
+}
